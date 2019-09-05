a7bd9c479626c18b21cc322cd74c9439ef4db1b3
narayanatutorial <narayanaswamy.ragi@gmail.com>
2019-09-05 19:07:37 +0530

    initial commit

---
+++ src/main/java/com/narayanatutorial/SpringBootStarterExample/ControllerExample.java
@@ -0,0 +1,9 @@
+package com.narayanatutorial.SpringBootStarterExample;
+
+
+public class ControllerExample {
+
+	public static void main(String args[]) {
+		System.out.println("ControllerExample ...");
+	}
+}
---
+++ src/main/java/com/narayanatutorial/SpringBootStarterExample/SpringBootStarterExampleApplication.java
@@ -0,0 +1,15 @@
+package com.narayanatutorial.SpringBootStarterExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterExampleApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(SpringBootStarterExampleApplication.class, args);
+		
+	}
+
+}
